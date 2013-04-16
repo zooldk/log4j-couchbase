664b54be00e44f40873167504010917954c2c041
Steffen Larsen <slarsen@braintrust.dk>
2013-04-16 10:34:33 +0200

    Clean up. removed sysouts.

--- src/main/java/dk/braintrust/os/logger/CouchBaseLogAppender.java
+++ src/main/java/dk/braintrust/os/logger/CouchBaseLogAppender.java
@@ -1,4 +1,5 @@
 package dk.braintrust.os.logger;
+
 import java.io.IOException;
 import java.net.URI;
 import java.util.LinkedList;
@@ -11,21 +12,21 @@ import com.couchbase.client.CouchbaseClient;
 
 /**
  * @author Steffen Larsen (slarsen@braintrust.dk)
- *
+ * 
  */
 public class CouchBaseLogAppender extends AppenderSkeleton {
 	private String hosts = "localhost";
 	private int port = 8092;
 	private String password = "";
 	private String defaultMetadataBucket = "default";
-	private Boolean developmentMode = true;	
-		
-	//private Map<String, String> messages; //TODO: store the log object into a temp memtable..
+	private Boolean developmentMode = true;
+
+	// private Map<String, String> messages; //TODO: store the log object into a
+	// temp memtable..
 	private CouchbaseClient client = null;
-	
-	private static List<URI> uris = new LinkedList<URI>(); 
-	
-	
+
+	private static List<URI> uris = new LinkedList<URI>();
+
 	@Override
 	public void activateOptions() {
 		super.activateOptions();
@@ -35,55 +36,53 @@ public class CouchBaseLogAppender extends AppenderSkeleton {
 			e.printStackTrace();
 		}
 		// TODO: setup mem queue here..
-		
+
 	}
-	
+
 	private CouchbaseClient setupCouchbase() throws IOException {
 		if (developmentMode)
 			System.setProperty("viewmode", "development");
-		
-		System.out.println("couchbase viewmode set to: " + System.getProperty("viewmode"));
-		System.out.println("metadata bucket for couchbase: " + defaultMetadataBucket);
-		
-		String[] couchServers = hosts.split(",");		
+
+		String[] couchServers = hosts.split(",");
 		for (String srv : couchServers) {
-			System.out.println("Server search for couchbase: " + srv);
 			uris.add(URI.create("http://" + srv + ":" + port + "/pools"));
-		}					
-		return new CouchbaseClient(uris,defaultMetadataBucket, password);
+		}
+		return new CouchbaseClient(uris, defaultMetadataBucket, password);
 	}
-	
+
 	@Override
 	protected void append(LoggingEvent event) {
-		String key = event.getLoggerName()+ "_" + event.getThreadName() +  "_" + event.timeStamp;
-		System.out.println(key + " persisted");		
+		String key = event.getLoggerName() + "_" + event.getThreadName() + "_" + event.timeStamp;
+		// System.out.println(key + " persisted");
 		// TODO: find better key for couchbase lookup
 		client.add(key, 0, this.layout.format(event));
-		
+
 	}
-	
-	
 
 	public void setDefaultMetadataBucket(String defaultMetadataBucket) {
 		this.defaultMetadataBucket = defaultMetadataBucket;
 	}
+
 	public void setHosts(String hosts) {
 		this.hosts = hosts;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public void setPort(int port) {
 		this.port = port;
 	}
+
 	public void setDevelopmentMode(Boolean developmentMode) {
 		this.developmentMode = developmentMode;
 	}
-	
+
 	public boolean requiresLayout() {
 		return true;
 	}
-	
+
 	public void close() {
 		client.shutdown();
 	}
--- src/main/java/dk/braintrust/os/logger/JsonEventLayout.java
+++ src/main/java/dk/braintrust/os/logger/JsonEventLayout.java
@@ -14,88 +14,89 @@ import org.apache.log4j.spi.ThrowableInformation;
 import com.google.gson.Gson;
 
 /**
- * This layout is based on https://github.com/lusis/log4j-jsonevent-layout/blob/master/src/main/java/net/logstash/log4j/JSONEventLayout.java#L10
+ * This layout is based on
+ * https://github.com/lusis/log4j-jsonevent-layout/blob/master
+ * /src/main/java/net/logstash/log4j/JSONEventLayout.java#L10
+ * 
  * @author Steffen Larsen (slarsen@braintrust.dk)
- *
+ * 
  */
 public class JsonEventLayout extends Layout {
-    private static Gson gson = new Gson();
-    private String ndc;
-    private Map mdc;
-    private LocationInfo info;
-    private HashMap<String, Object> fieldData;
-    private HashMap<String, Object> exceptionInformation;
+	private static Gson gson = new Gson();
+	private String ndc;
+	private Map mdc;
+	private LocationInfo info;
+	private HashMap<String, Object> fieldData;
+	private HashMap<String, Object> exceptionInformation;
 
-    
 	public void activateOptions() {
-		
+
 	}
 
 	@Override
 	public String format(LoggingEvent event) {
 		info = event.getLocationInformation();
-        fieldData = new HashMap<String, Object>();
-        exceptionInformation = new HashMap<String, Object>();
-        mdc = event.getProperties();
-        ndc = event.getNDC();
+		fieldData = new HashMap<String, Object>();
+		exceptionInformation = new HashMap<String, Object>();
+		mdc = event.getProperties();
+		ndc = event.getNDC();
 
-        JsonLogObj obj = new JsonLogObj();		
-        obj.setHostname(getHostName());
-        obj.setMsg(event.getRenderedMessage());
-        obj.setTimestamp(event.getTimeStamp());
-        obj.setThread(event.getThreadName());
-        if(event.getThrowableInformation() != null) {
-            final ThrowableInformation throwableInformation = event.getThrowableInformation();
-            if(throwableInformation.getThrowable().getClass().getCanonicalName() != null){
-                exceptionInformation.put("exception_class",throwableInformation.getThrowable().getClass().getCanonicalName());
-            }
-            if(throwableInformation.getThrowable().getMessage() != null) {
-                exceptionInformation.put("exception_message",throwableInformation.getThrowable().getMessage());
-            }
-            if( throwableInformation.getThrowableStrRep() != null) {
-                String stackTrace = StringUtils.join(throwableInformation.getThrowableStrRep(),"\n");
-                exceptionInformation.put("stacktrace",stackTrace);
-            }
-            addFieldData("exception",exceptionInformation);
-        }
+		JsonLogObj obj = new JsonLogObj();
+		obj.setHostname(getHostName());
+		obj.setMsg(event.getRenderedMessage());
+		obj.setTimestamp(event.getTimeStamp());
+		obj.setThread(event.getThreadName());
+		if (event.getThrowableInformation() != null) {
+			final ThrowableInformation throwableInformation = event.getThrowableInformation();
+			if (throwableInformation.getThrowable().getClass().getCanonicalName() != null) {
+				exceptionInformation.put("exception_class",throwableInformation.getThrowable().getClass().getCanonicalName());
+			}
+			if (throwableInformation.getThrowable().getMessage() != null) {
+				exceptionInformation.put("exception_message",throwableInformation.getThrowable().getMessage());
+			}
+			if (throwableInformation.getThrowableStrRep() != null) {
+				String stackTrace = StringUtils.join(throwableInformation.getThrowableStrRep(), "\n");
+				exceptionInformation.put("stacktrace", stackTrace);
+			}
+			addFieldData("exception", exceptionInformation);
+		}
 
+		if (event.locationInformationExists()) {
+			info = event.getLocationInformation();
+			addFieldData("file", info.getFileName());
+			addFieldData("line_number", info.getLineNumber());
+			addFieldData("class", info.getClassName());
+			addFieldData("method", info.getMethodName());
+		}
 
-        if(event.locationInformationExists()) {
-            info = event.getLocationInformation();
-            addFieldData("file",info.getFileName());
-            addFieldData("line_number",info.getLineNumber());
-            addFieldData("class",info.getClassName());
-            addFieldData("method",info.getMethodName());
-        }
+		addFieldData("mdc", mdc);
+		addFieldData("ndc", ndc);
+		addFieldData("level", event.getLevel().toString());
 
-        addFieldData("mdc",mdc);
-        addFieldData("ndc",ndc);
-        addFieldData("level",event.getLevel().toString());
-
-        obj.setExceptionInformation(exceptionInformation);
-        obj.setFieldData(fieldData);        
-        return gson.toJson(obj);
+		obj.setExceptionInformation(exceptionInformation);
+		obj.setFieldData(fieldData);
+		return gson.toJson(obj);
 	}
 
 	@Override
 	public boolean ignoresThrowable() {
 		return false;
 	}
-	
+
 	private String getHostName() {
-		 InetAddress addr = null;
+		InetAddress addr = null;
 		try {
 			addr = InetAddress.getLocalHost();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-         return addr.getHostName();
+		return addr.getHostName();
 	}
-	
-	 private void addFieldData(String keyname, Object keyval){
-	        if(null != keyval){
-	            fieldData.put(keyname, keyval);
-	        }
-	    }
+
+	private void addFieldData(String keyname, Object keyval) {
+		if (null != keyval) {
+			fieldData.put(keyname, keyval);
+		}
+	}
 }
